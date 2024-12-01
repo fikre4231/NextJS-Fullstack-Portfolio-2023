36907302ec77eb558df98d9251a7f0421eaceb1a
fikre4231 <fikrebereda@gmail.com>
2024-12-01 12:11:15 -0800

    add new mongo db

--- src/database/index.js
+++ src/database/index.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export default async function connectToDB() {
   try {
     await mongoose.connect(
-      "mongodb+srv://sangam:sangam2023@cluster0.7aauitj.mongodb.net/"
+      " mongodb+srv://besufekadfikre456:97Xbt07c7AiJaSzX@cluster0.z2i8s.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     );
     console.log("Database connected successfully");
   } catch (e) {
